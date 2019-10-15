8c8fae8617f23086bd637fbffd1a8d1543b71861
sapeled3 <oyinkuromosesvictor@outlook.com>
2019-10-15 22:45:25 +0100

    setup utility for backend middleware

---
+++ src/util/theme.js
@@ -0,0 +1,38 @@
+export default {
+    palette: {
+      primary: {
+        main: "#006064",
+        contrastText: "#fff"
+      },
+      secondary: {
+        main: "#14a37f",
+        contrastText: "#fff"
+      }
+    },
+    typography: {
+      useNextVariants: true
+    },
+    overrides : {
+      form: {
+        textAlign: "center"
+      },
+      pageTitle: {
+        margin: "20px auto 20px auto"
+      },
+      textField: {
+        margin: "10px auto 10px auto"
+      },
+      button: {
+        marginTop: 20,
+        postion: "relative"
+      },
+      customError: {
+        marginTop: 5,
+        color: "red",
+        fontSize: "0.8rem"
+      },
+      progress: {
+        position: "absolute"
+      }
+    }
+  }
